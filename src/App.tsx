14e178ab59be18081d9444e186cade46c9fbdbd1
someisakura <m-shimizu0725@g.ecc.u-tokyo.ac.jp>
2024-12-28 17:03:06 +0900

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+// App.tsx
+
+import Title from "./components/Title"
+import Form from "./components/Form"
+import Results from "./components/Results"
+import { useState } from "react"
+import Loading from "./components/Loading"
+
+type ResultsState = {
+  country: string
+  cityName: string
+  temperature: string
+  conditionText: string
+  icon: string
+}
+
+const App = () => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [city, setCity] = useState<string>("")
+  const [results, setResults] = useState<ResultsState>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: ""
+  })
+
+  const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      setLoading(true)
+      fetch(`https://api.weatherapi.com/v1/current.json?key=db8befbcd40b42c9a5a65133242309&q=${city}&aqi=no`)
+      .then(res => res.json())
+      .then(data => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon
+        })
+        setLoading(false)
+        setCity("")
+      })
+      .catch(() => alert("エラーが発生しました。ページをリロードして、もう一度お試しください。"))
+  }
+  return(
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form 
+        setCity={setCity}
+        getWeather={getWeather}
+        city={city}
+        />
+        {loading ? <Loading/> : <Results results={results}/>}
+      </div>
+    </div>
+  )
+}
+
+export default App
